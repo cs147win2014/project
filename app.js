e1bcd9f5c4a5e528bf304689f8d8355dda829564
mnburkle <mnburkle@stanford.edu>
2014-02-06 15:21:31 -0800

    added viewAssignments to the pages that app.js keeps track of

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var add = require ('./routes/add');
+var viewAssignments = require('./routes/viewAssignments');
 // Example route
 // var user = require('./routes/user');
 
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/add',add.addFriend);
+app.get('/viewAssignments',viewAssignments.view);
 // Example route
 // app.get('/users', user.list);
 
