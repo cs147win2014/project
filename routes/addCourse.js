2db39ec23d6d47d02d5f13a84fa00b5376b3927c
mnburkle <mnburkle@stanford.edu>
2014-02-15 00:48:53 -0800

    in the process of changing addCourse, moving towards a walk through ending with a server request, as opposed to multiple get/posts

--- routes/addCourse.js
+++ routes/addCourse.js
@@ -3,15 +3,11 @@ var data = require("../data.json");
 exports.add = function(req, res) {    
 	// Your code goes here%
 	var newCourse = {department: req.query.department, 
-					 number: req.query.number, 
-					 title: req.query.title,
-					 professor: req.query.professor,
-					 quarter: req.query.quarter,
-					 year: req.query.year};
+					 number: req.query.number};
 
 	data["courses"].push(newCourse);
 	
 	console.log(newCourse);
 	// instead of rendering add screen, rendered data screen
-	res.render('addCourse',data);
+	res.render('viewCourses',data);
  }
