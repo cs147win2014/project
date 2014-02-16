6cce20f279ec6b78d2d697ecbfdd4adb258befa7
mnburkle <mnburkle@stanford.edu>
2014-02-15 21:18:53 -0800

    changed addCourse to POST not GET

--- routes/addCourse.js
+++ routes/addCourse.js
@@ -2,12 +2,12 @@ var data = require("../data.json");
 
 exports.add = function(req, res) {    
 	// Your code goes here%
-	var newCourse = {department: req.query.department, 
-					 number: req.query.number};
+	var newCourse = {department: req.body.department, 
+					 number: req.body.number};
 
 	data["courses"].push(newCourse);
 	
 	console.log(newCourse);
 	// instead of rendering add screen, rendered data screen
-	res.render('viewCourses',data);
+	res.render('editCourse',data);
  }
