64ce2abc56cf57d977f0bd8048ac5fe2501457e2
mnburkle <mnburkle@stanford.edu>
2014-02-20 00:35:50 -0800

    fixing button to add course

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$('#addCourseButton').click(addCourse);
+
+}
+
+function addCourse() {
+	$.get('addACourse');
+} 
+
+
