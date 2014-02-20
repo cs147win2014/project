4efc723161ebfce1febdb6fceaa975e3ba2c9111
mnburkle <mnburkle@stanford.edu>
2014-02-19 23:13:43 -0800

    working on schema still not sure if correct!

--- models.js
+++ models.js
@@ -5,28 +5,28 @@ var Mongoose = require('mongoose');
 
 var AssignmentTypeSchema = new Mongoose.Schema({
   "name": String,
-  "percent": Number
+  "weighting": Number
 });
 
 
 
 var SyllabusSchema = new Mongoose.Schema({
-  "assignments": [AssignmentTypeSchema]
+  "assignmentTypes": Array // array of assignment types
 });
 
 
 var CourseSchema = new Mongoose.Schema({
   "name": String,
-  "number": Number,
   "department": String,
-  "syllabus": [SyllabusSchema]
+  "number": Number,
+  "syllabus": Objectid // object id corresponding to the syllabus of the class
 });
 
 
 var UserSchema = new Mongoose.Schema({
   "name": String,
   "password": String,
-  "classes": [CourseSchema]
+  "classes": Array // array of object ids corresponding to the classes they are in
 });
 
 
