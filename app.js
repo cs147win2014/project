9fd0aed810ce3bfa7a7fa27a66f5bbf54729a727
Whitney LaRow <wlarow@stanford.edu>
2014-02-19 23:54:57 -0800

    ?

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get('/signup',function(req, res) {
 
 app.post('/signup',signup.signIn);
 app.post('/editCourse',editCourse.addSyllabusFields)
-app.post('/addACourse/new', course.add);
+app.post('/addACourse/new',course.add);
 
 app.post('/signup/check/username', function(req, res) {
   var username = req.body.username;
