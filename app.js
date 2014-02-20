cb47ffe13f83104e5d97a6e78738ce198e7918e4
Whitney LaRow <wlarow@stanford.edu>
2014-02-19 16:36:42 -0800

    added models

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ var signup = require('./routes/signup');
 // Example route
 // var user = require('./routes/user');
 
+// Connect to the Mongo database, whether locally or on Heroku
+// MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
+var local_database_name = 'project';
+var local_database_uri  = 'mongodb://localhost/' + local_database_name
+var database_uri = process.env.MONGOLAB_URI || local_database_uri
+mongoose.connect(database_uri);
+
 var app = express();
 
 // all environments
