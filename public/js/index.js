60e7a7b66ea70a8e7e9f5affa2a3256e9fb8fd80
mnburkle <mnburkle@stanford.edu>
2014-02-20 22:42:42 -0800

    changed function names for clarity, added in show assignments which hasn’t actually been tested yet!

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,17 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$('#addCourseButton').click(addCourse);
-	$('.courseButton').click(viewCourse);
+	$('.courseButton').click(viewCourseSyllabus);
 
 }
+
 
 function addCourse() {
 	console.log("user clicked button");
@@ -21,18 +22,19 @@ function addCourse() {
 	//$.get("/addACourse");
 } 
 
-function viewCourse(e) {
+function viewCourseSyllabus(e) {
 	e.preventDefault();
+
 	// Get the div ID, e.g., "project3"
 	var courseName = $(this).closest("li").attr('id');
 	//alert(courseName);
 	// get rid of 'project' from the front of the id 'project3'
 	
 	var url = "/courses/" + courseName;
-	$.get(url, showCourseInfo);
+	$.get(url, showCourseSyllabus);
 } 
 
-function showCourseInfo(result) {
+function showCourseSyllabus(result) {
 
 	var syllabusDiv = $("#"+result.department+result.number+"syllabus");
 
@@ -87,6 +89,50 @@ function showCourseInfo(result) {
 		$("#viewButton"+result.department+result.number).text("Less");
 	}
 	
+}
+
+function showCourseAssignments(results) {
+	var $newAssignmentsDiv = $("<div>", {id: result.department+result.number+"assignments"});
+
+	var $table = $("<table>", {class: "table"});
+	var $nameheader = $("<th>", {innerHTML: "Name"});
+	var $typeheader = $("<th>", {innerHTML: "Type"});
+	var $scoreheader= $("<th>", {innerHTML: "Score"});
+	var $totalheader= $("<th>", {innerHTML: "Total"});
+
+	($table).append($nameheader);
+	($table).append($typeheader);
+	($table).append($scoreheader);
+	($table).append($totalheader);
+
+	for(var i=0; i<result.assignments.length; i++) {
+		var $tr = $("<tr>", {id: "assignment"+i})
+		var currentAssignment = result.assignments[i];
+		var name = currentAssignment.name;
+		var type = currentAssignment.type;
+		var score = currentAssignment.score;
+		var total = currentAssignment.total;
+		var course = currentAssignment.course;
+		if(course===result.department+result.number) {
+			// print it, it's for the right course in theory we should filter
+			// these out before we even return 'results'
+			var $tdName = $("<td>");
+			$tdName.text(name);
+			var $tdType = $("<td>");
+			$tdType.text(type);
+			var $tdScore = $("<td>");
+			$tdScore.text(score);
+			var $tdTotal = $("<td>");
+			$tdTotal.text(total);
+			($tr).append($tdName);
+			($tr).append($tdType);
+			($tr).append($tdScore);
+			($tr).append($tdTotal);
+			($table).append($tr);
+		}
+	}
+	($newAssignmentsDiv).append($table);
+	$("#"+result.department+result.number).append($newAssignmentsDiv);
 }
 
 
