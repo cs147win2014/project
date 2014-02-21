56e4c03ab951fd450919266fb4ef0665b2ba4b04
mnburkle <mnburkle@stanford.edu>
2014-02-20 23:47:56 -0800

    fixed multiple fields thingie for syllabi

--- public/js/editSyllabus.js
+++ public/js/editSyllabus.js
@@ -1,26 +1,33 @@
 $(document).ready(function(){
     var next = 1;
+
     $(".add-more").click(function(e){
         e.preventDefault();
+        // the div element selectors
         var addto = "#field" + next;
         var addRemove = "#field" + (next);
+
         next = next + 1;
         $("numFields").attr("value", next);
 
-        var newIn = '<input autocomplete="off" placeholder="Ex: \'Homework\'" class="form-control" id="field' + next + '" name="field' + next + '" type="text">';
+        var newIn = '<div id="field' + next + '" name="field' + next + '" class="col-xs-8 col-md-8"><br><input autocomplete="off" placeholder="Ex: Homework" class="form-control" type="text"></div>';
         var newInput = $(newIn);
-        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field" class="input-append col-md-4">';
+
+        var removeBtn = '<div id="remove' + (next - 1) + '" class="col-xs-4 col-md-4 removeButton"><br><button class="btn btn-danger remove-me" ><i class="glyphicon glyphicon-minus"></i></button></div></div><div id="field" class="input-append row">';
         var removeButton = $(removeBtn);
+
         $(addto).after(newInput);
         $(addRemove).after(removeButton);
         $("#field" + next).attr('data-source',$(addto).attr('data-source'));
         $("#count").val(next);  
         
             $('.remove-me').click(function(e){
+                //alert('clicked!');
                 e.preventDefault();
-                var fieldNum = this.id.charAt(this.id.length-1);
+                var $divToRemove = $(this).closest('div'); //.find(".disabled");
+                var fieldNum = ($divToRemove).attr('id').charAt(($divToRemove).attr('id').length-1);
                 var fieldID = "#field" + fieldNum;
-                $(this).remove();
+                ($divToRemove).remove();
                 $(fieldID).remove();
             });
     });
