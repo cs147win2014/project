1037f48a21fe697ee9b9cc920c904d48d7945b82
mnburkle <mnburkle@stanford.edu>
2014-02-27 00:33:36 -0800

    removed extraneous files and functions from course.js

--- routes/course.js
+++ routes/course.js
@@ -14,16 +14,10 @@ exports.addAssignment = function(req, res) { 
 	res.render('add',data);
  }
 
-// Get all of our friend data
-var data = require('../data.json');
-
 exports.view = function(req, res){
 	console.log(data);
 	res.render('course',data);
 };
-
-
-var data = require("../data.json");
 
 exports.addAssignment = function(req, res) {    
 	// Your code goes here%
@@ -39,12 +33,6 @@ exports.addAssignment = function(req, res) { 
 	res.render('editCourse',data);
  };
 
-exports.view = function(req, res){
-	
-	console.log(data);
-	res.render('viewCourses',data);
-};
-
 exports.addSyllabusFields = function(req, res) {
 	allFields = req.body;
 
@@ -57,9 +45,6 @@ exports.addSyllabusFields = function(req, res) {
 	res.render('editCourse',data);
 	return;
 };
-
-
-var data = require("../data.json");
 
 exports.add = function(req, res) {    
 	// Your code goes here%
@@ -112,54 +97,3 @@ exports.viewCourseInfo = function(req, res) { 
 	//res.render('/courses/'+courseName,data);
 	
  }
-
-
-
-// var models = require('../models');
-
-// /*
-//  * GET home page.
-//  */
-
-// exports.view = function(req, res){
-
-// 	models.User
-// 		.find()
-// 		.sort('name')
-// 		.exec(renderUsers);
-
-// 	function renderUsers(err, users) {
-// 		res.render('index', { 'users': users });
-// 	}
-
-
-// 	models.Course
-// 		.find()
-// 		.sort('department')
-// 		.exec(renderCourse);
-
-// 	function renderCourse(err, course) {
-// 		res.render('index', { 'course': course });
-// 	}
-
-
-// 	models.Syllabus
-// 		.find()
-// 		.sort('assignments')
-// 		.exec(renderSyllabus);
-
-// 	function renderSyllabus(err, syllabus) {
-// 		res.render('index', { 'syllabus': syllabus });
-// 	}
-
-
-// 	models.AssignmentType
-// 		.find()
-// 		.sort('name')
-// 		.exec(renderAssignmentType);
-
-// 	function renderAssignmentType(err, assignmentType) {
-// 		res.render('index', { 'assignmentType': assignmentType });
-// 	}
-
-// };
