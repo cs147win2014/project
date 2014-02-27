3f018fa1a855b0f945beaff77c1803b0acde55df
mnburkle <mnburkle@stanford.edu>
2014-02-27 02:09:26 -0800

    added in button to call server to add assignment

--- public/js/addAssignment.js
+++ public/js/addAssignment.js
@@ -11,5 +11,14 @@ function initializePage() {
     $('#courseSelect').on('change', function(event) {
         $('#typeSelect').show();
     });
+
+    $('#addAssignmentBtn').click(addAssignment);
+}
+
+function addAssignment() {
+	console.log("user clicked add assignment button");
+	window.location.href = "/addAssignment"; // load the page
+
+	//$.get("/addACourse");
 }
 
