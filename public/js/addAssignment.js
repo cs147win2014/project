b3dca630c10dd74409380c459a560f5ef4f8353d
Isabelle Woodrow <iwoodrow@stanford.edu>
2014-02-27 01:06:19 -0800

    Isabelle trying to fix addAssignment added dropdowns

---
+++ public/js/addAssignment.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+
+function initializePage() {
+	$('#typeSelect').hide();
+    $('#courseSelect').on('change', function(event) {
+        $('#typeSelect').show();
+    });
+}
+
