d3220769404afc16bb5205ed36e8dbfc1966756a
mnburkle <mnburkle@stanford.edu>
2014-02-27 01:09:27 -0800

    add course dialog has back button

--- public/js/addCourse.js
+++ public/js/addCourse.js
@@ -9,24 +9,32 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('#courseSubmitBtn').click(addCourse);
+	$('#backBtn').click(goBack);
+	$('#submitBtn').click(addCourse);
+
+}
+
+function addCourse() {
+	console.log('clicked');
+	var name = $('#new-course-form #name').val();
+	var department = $('#new-course-form #department').val();
+	var number = $('#new-course-form #number').val();
+	var syllabus = $('#new-course-form #syllabus').val();
+	var json = {
+		'name': title,
+		'department': image_url,
+		'number':  date,
+		'syllabus': summary
+	};
+	$.post('/addACourse/new', json, function() {
+		window.location.href = 'index'; // reload the page
+	});
+}
 
 
-	function addCourse() {
-		console.log('clicked');
-		var name = $('#new-course-form #name').val();
-		var department = $('#new-course-form #department').val();
-		var number = $('#new-course-form #number').val();
-		var syllabus = $('#new-course-form #syllabus').val();
-		var json = {
-			'name': title,
-			'department': image_url,
-			'number':  date,
-			'syllabus': summary
-		};
-		$.post('/addACourse/new', json, function() {
-			window.location.href = 'index'; // reload the page
-		});
-	} 
+
+function goBack(e) {
+	e.preventDefault();
+	window.location.href = "/index";
 }
 
