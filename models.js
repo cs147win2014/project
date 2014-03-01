1563b3a1da64b8f50fe1424acd3bb074f25965e3
mnburkle <mnburkle@stanford.edu>
2014-03-01 13:52:29 -0800

    changed course ‘number’ to type String. for example, course department = “CS”, course number = “106A” if the course number has a letter in it, it would mess up in type casting. thus it should just be a string

--- models.js
+++ models.js
@@ -19,7 +19,7 @@ var AssignmentSchema = new Mongoose.Schema({
 var CourseSchema = new Mongoose.Schema({
   "name": String,
   "department": String,
-  "number": Number,
+  "number": String,
   // object id corresponding to the syllabus of the class
   "syllabus": [{ type: Mongoose.Schema.Types.ObjectId, ref: 'AssignmentType' }]
 });
