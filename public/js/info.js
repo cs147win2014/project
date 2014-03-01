3be4757ddc32319b74ef7419243ffcd49cdb1983
mnburkle <mnburkle@stanford.edu>
2014-02-28 17:50:00 -0800

    playing with charts

--- public/js/info.js
+++ public/js/info.js
@@ -3,189 +3,280 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
     console.log('document is ready');
+    var myData;
 
     $.get("/getAssignments", function(data) {
         console.log(data);
-        // data is a JavaScript object now. Handle it as such
+        console.log('now ill try to assign');
 
-    });
-
-
-    var chart = AmCharts.makeChart("chartdiv", {
-        "type": "serial",
-        "theme": "none",
-        "pathToImages": "http://www.amcharts.com/lib/3/images/",
-        "legend": {
-            "equalWidths": false,
-            "periodValueText": "total: [[value.sum]]",
-            "position": "top",
-            "valueAlign": "left",
-            "valueWidth": 100
-        },
-        "dataProvider": [{
-            "year": 1994,
-            "cars": 1587,
-            "motorcycles": 650,
-            "bicycles": 121
-        }, {
-            "year": 1995,
-            "cars": 1567,
-            "motorcycles": 683,
-            "bicycles": 146
-        }, {
-            "year": 1996,
-            "cars": 1617,
-            "motorcycles": 691,
-            "bicycles": 138
-        }, {
-            "year": 1997,
-            "cars": 1630,
-            "motorcycles": 642,
-            "bicycles": 127
-        }, {
-            "year": 1998,
-            "cars": 1660,
-            "motorcycles": 699,
-            "bicycles": 105
-        }, {
-            "year": 1999,
-            "cars": 1683,
-            "motorcycles": 721,
-            "bicycles": 109
-        }, {
-            "year": 2000,
-            "cars": 1691,
-            "motorcycles": 737,
-            "bicycles": 112
-        }, {
-            "year": 2001,
-            "cars": 1298,
-            "motorcycles": 680,
-            "bicycles": 101
-        }, {
-            "year": 2002,
-            "cars": 1275,
-            "motorcycles": 664,
-            "bicycles": 97
-        }, {
-            "year": 2003,
-            "cars": 1246,
-            "motorcycles": 648,
-            "bicycles": 93
-        }, {
-            "year": 2004,
-            "cars": 1318,
-            "motorcycles": 697,
-            "bicycles": 111
-        }, {
-            "year": 2005,
-            "cars": 1213,
-            "motorcycles": 633,
-            "bicycles": 87
-        }, {
-            "year": 2006,
-            "cars": 1199,
-            "motorcycles": 621,
-            "bicycles": 79
-        }, {
-            "year": 2007,
-            "cars": 1110,
-            "motorcycles": 210,
-            "bicycles": 81
-        }, {
-            "year": 2008,
-            "cars": 1165,
-            "motorcycles": 232,
-            "bicycles": 75
-        }, {
-            "year": 2009,
-            "cars": 1145,
-            "motorcycles": 219,
-            "bicycles": 88
-        }, {
-            "year": 2010,
-            "cars": 1163,
-            "motorcycles": 201,
-            "bicycles": 82
-        }, {
-            "year": 2011,
-            "cars": 1180,
-            "motorcycles": 285,
-            "bicycles": 87
-        }, {
-            "year": 2012,
-            "cars": 1159,
-            "motorcycles": 277,
-            "bicycles": 71
-        }],
-        "valueAxes": [{
-            "stackType": "regular",
-            "gridAlpha": 0.07,
-            "position": "left",
-            "title": "Traffic incidents"
-        }],
-        "graphs": [{
-            //"balloonText": "<img src='http://www.amcharts.com/lib/3/images/car.png' style='vertical-align:bottom; margin-right: 10px; width:28px; height:21px;'><span style='font-size:14px; color:#000000;'>[[value]]</span>",
-            "fillAlphas": 0.6,
-            //"hidden": true,
-            "lineAlpha": 0.4,
-            "title": "Cars",
-            "valueField": "cars"
-        }, {
-            //"balloonText": "<span style='font-size:14px; color:#000000;'>[[value]]</span>",
-            "fillAlphas": 0.6,
-            "lineAlpha": 0.4,
-            "title": "Motorcycles",
-            "valueField": "motorcycles"
-        }, {
-            //"balloonText": "<span style='font-size:14px; color:#000000;'>[[value]]</span>",
-            "fillAlphas": 0.6,
-            "lineAlpha": 0.4,
-            "title": "Bicycles",
-            "valueField": "bicycles"
-        }],
-        "plotAreaBorderAlpha": 0,
-        "marginTop": 10,
-        "marginLeft": 0,
-        "marginBottom": 0,
-        "chartScrollbar": {},
-        "chartCursor": {
-            "cursorAlpha": 0
-        },
-        "categoryField": "year",
-        "categoryAxis": {
-            "startOnAxis": true,
-            "axisColor": "#DADADA",
-            "gridAlpha": 0.07//,
-            /*"guides": [{
-                category: "2001",
-                toCategory: "2003",
-                lineColor: "#CC0000",
-                lineAlpha: 1,
-                fillAlpha: 0.2,
-                fillColor: "#CC0000",
-                dashLength: 2,
-                inside: true,
-                labelRotation: 90,
-                label: "fines for speeding increased"
+        var chart = AmCharts.makeChart("testLine", {
+            "type": "serial",
+            "theme": "none",
+            "marginLeft": 20,
+            "pathToImages": "http://www.amcharts.com/lib/3/images/",
+            "dataProvider": [{
+                "year": "1950",
+                "value": -0.307
             }, {
-                category: "2007",
-                lineColor: "#CC0000",
-                lineAlpha: 1,
-                dashLength: 2,
-                inside: true,
-                labelRotation: 90,
-                label: "motorcycle fee introduced"
-            }]*/
-        },
-        "exportConfig": {
-            "menuTop": "10px",
-            "menuRight": "10px",
-            "menuItems": [{
-                "icon": 'http://www.amcharts.com/lib/3/images/car.png',
-                "format": 'png'
-            }]
-        }
+                "year": "1951",
+                "value": -0.168
+            }, {
+                "year": "1952",
+                "value": -0.073
+            }, {
+                "year": "1953",
+                "value": -0.027
+            }, {
+                "year": "1954",
+                "value": -0.251
+            }, {
+                "year": "1955",
+                "value": -0.281
+            }, {
+                "year": "1956",
+                "value": -0.348
+            }, {
+                "year": "1957",
+                "value": -0.074
+            }, {
+                "year": "1958",
+                "value": -0.011
+            }, {
+                "year": "1959",
+                "value": -0.074
+            }, {
+                "year": "1960",
+                "value": -0.124
+            }, {
+                "year": "1961",
+                "value": -0.024
+            }, {
+                "year": "1962",
+                "value": -0.022
+            }, {
+                "year": "1963",
+                "value": 0
+            }, {
+                "year": "1964",
+                "value": -0.296
+            }, {
+                "year": "1965",
+                "value": -0.217
+            }, {
+                "year": "1966",
+                "value": -0.147
+            }, {
+                "year": "1967",
+                "value": -0.15
+            }, {
+                "year": "1968",
+                "value": -0.16
+            }, {
+                "year": "1969",
+                "value": -0.011
+            }, {
+                "year": "1970",
+                "value": -0.068
+            }, {
+                "year": "1971",
+                "value": -0.19
+            }, {
+                "year": "1972",
+                "value": -0.056
+            }, {
+                "year": "1973",
+                "value": 0.077
+            }, {
+                "year": "1974",
+                "value": -0.213
+            }, {
+                "year": "1975",
+                "value": -0.17
+            }, {
+                "year": "1976",
+                "value": -0.254
+            }, {
+                "year": "1977",
+                "value": 0.019
+            }, {
+                "year": "1978",
+                "value": -0.063
+            }, {
+                "year": "1979",
+                "value": 0.05
+            }, {
+                "year": "1980",
+                "value": 0.077
+            }, {
+                "year": "1981",
+                "value": 0.12
+            }, {
+                "year": "1982",
+                "value": 0.011
+            }, {
+                "year": "1983",
+                "value": 0.177
+            }, {
+                "year": "1984",
+                "value": -0.021
+            }, {
+                "year": "1985",
+                "value": -0.037
+            }, {
+                "year": "1986",
+                "value": 0.03
+            }, {
+                "year": "1987",
+                "value": 0.179
+            }, {
+                "year": "1988",
+                "value": 0.18
+            }, {
+                "year": "1989",
+                "value": 0.104
+            }, {
+                "year": "1990",
+                "value": 0.255
+            }, {
+                "year": "1991",
+                "value": 0.21
+            }, {
+                "year": "1992",
+                "value": 0.065
+            }, {
+                "year": "1993",
+                "value": 0.11
+            }, {
+                "year": "1994",
+                "value": 0.172
+            }, {
+                "year": "1995",
+                "value": 0.269
+            }, {
+                "year": "1996",
+                "value": 0.141
+            }, {
+                "year": "1997",
+                "value": 0.353
+            }, {
+                "year": "1998",
+                "value": 0.548
+            }, {
+                "year": "1999",
+                "value": 0.298
+            }, {
+                "year": "2000",
+                "value": 0.267
+            }, {
+                "year": "2001",
+                "value": 0.411
+            }, {
+                "year": "2002",
+                "value": 0.462
+            }, {
+                "year": "2003",
+                "value": 0.47
+            }, {
+                "year": "2004",
+                "value": 0.445
+            }, {
+                "year": "2005",
+                "value": 0.47
+            }],
+            "valueAxes": [{
+                "axisAlpha": 0,
+                "inside": true,
+                "position": "left",
+                "ignoreAxisWidth": true
+            }],
+            "graphs": [{
+                "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
+                "bullet": "round",
+                "bulletSize": 6,
+                "lineColor": "#d1655d",
+                "lineThickness": 2,
+                "negativeLineColor": "#637bb6",
+                "type": "smoothedLine",
+                "valueField": "value"
+            }],
+            "chartScrollbar": {},
+            "chartCursor": {
+                "categoryBalloonDateFormat": "YYYY",
+                "cursorAlpha": 0,
+                "cursorPosition": "mouse"
+            },
+            "dataDateFormat": "YYYY",
+            "categoryField": "year",
+            "categoryAxis": {
+                "minPeriod": "YYYY",
+                "parseDates": true,
+                "minorGridAlpha": 0.1,
+                "minorGridEnabled": true
+            }
+        });
+        
+
+        var allAssignmentsChart = AmCharts.makeChart("allAssignmentsChartDiv", {
+            "type": "serial",
+            "theme": "none",
+            "pathToImages": "http://www.amcharts.com/lib/3/images/",
+            "legend": {
+                "equalWidths": false,
+                "periodValueText": "total: [[value.sum]]",
+                "position": "top",
+                "valueAlign": "left",
+                "valueWidth": 100
+            },
+
+            "dataProvider": data,
+
+            "valueAxes": [{
+                "stackType": "regular",
+                "gridAlpha": 0.07,
+                "position": "left",
+                "title": "Test plot! :D"
+            }],
+
+            "graphs": [
+                {
+                    "fillAlphas": 0.6,
+                    "lineAlpha": 0.4,
+                    "title": "Your score",
+                    "valueField": "score"
+                }, 
+                {
+                    "fillAlphas": 0.6,
+                    "hidden": true,
+                    "lineAlpha": 0.4,
+                    "title": "Possible score",
+                    "valueField": "total"
+                }
+            ],
+
+            "plotAreaBorderAlpha": 0,
+            "marginTop": 10,
+            "marginLeft": 0,
+            "marginBottom": 0,
+            "chartScrollbar": {},
+            "chartCursor": {
+                "cursorAlpha": 0
+            },
+            "categoryField": "name",
+            "categoryAxis": {
+                "startOnAxis": true,
+                "axisColor": "#DADADA",
+                "gridAlpha": 0.07
+            },
+            "exportConfig": {
+                "menuTop": "10px",
+                "menuRight": "10px",
+                "menuItems": [{
+                    "icon": 'http://www.amcharts.com/lib/3/images/car.png',
+                    "format": 'png'
+                }]
+            }
+        });
+
+        
     });
-    console.log('loaded chart');
 });
