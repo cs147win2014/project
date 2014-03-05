40a89856f3ab764015e19b395d5e54091857b43b
Whitney LaRow <wlarow@stanford.edu>
2014-03-04 23:32:09 -0800

    unclear

--- routes/view.js
+++ routes/view.js
@@ -25,11 +25,6 @@ exports.viewCoursePage = function(req, res) { 
       console.log(doc[0]);
       var results;
       results = doc[0];
-      // if (!doc.length) { //no result was found aka user isn't in the database aka user was "Guest"
-      //   results = data; 
-      // } else { //user is in the database, use their data
-      //   results = doc[0];
-      // }
       var hasCourses;
       if(doc[0].courses.length) { //false if no courses
         hasCourses = true;
@@ -46,73 +41,113 @@ exports.viewCoursePage = function(req, res) { 
       res.render('course',sessionData);
       return;
     });
-    
   } 
   else {
     console.log('couldnt find user ' + username);
     res.render('login',{error:"Could not find user " + username});
     return;
   }
-
-  //var number = req.params.number; 
-
-  // this is where we will go through data and systematically construct a JSON of
-  // all the data we need to render this page
-
-  // console.log("The course name is: " + courseName);
-  // console.log(courseData);
-  // res.render('course', courseData);
 };
+
+
 
 exports.viewAssignments = function(req, res){
-  data["progressTabFirst"] = false;
-  var user = req.session.user;
-  console.log(user);
-  if(!user) {
-    console.log('username undefined we should not do this');
-    res.render('login', {error:"Please sign in first!"});
-    return;
-  }
+  // var user = req.session.user;
+  // console.log(user);
+  // if(!user) {
+  //   console.log('username undefined we should not do this');
+  //   res.render('login', {error:"Please sign in first!"});
+  //   return;
+  // }
   
-  //query database - get array of json situations
-  var actualUser = models.User.find({"username": user});
-  if(actualUser.length != 0) {
-    actualUser.populate("courses")
-    .exec(function(err, doc) {
-      if(err) {console.log(err)};
-      console.log(doc[0]);
-      var results;
-      results = doc[0];
-      // if (!doc.length) { //no result was found aka user isn't in the database aka user was "Guest"
-      //   results = data; 
-      // } else { //user is in the database, use their data
-      //   results = doc[0];
-      // }
-      var hasCourses;
-      if(doc[0].courses.length) { //false if no courses
-        hasCourses = true;
-      } else {
-        hasCourses = false;
-      }
-      var sessionData = { "userData": results, "user": user, "expand": false, "hasCourses": hasCourses};
-      console.log("user data is " + sessionData);
-      res.render('viewAssignments',sessionData);
-      return;
-    });
-    
-  } 
-  else {
-    console.log('couldnt find user ' + username);
-    res.render('login',{error:"Could not find user " + username});
-    return;
-  }
+  // //query database - get array of json situations
+  // var actualUser = models.User.find({"username": user});
+  // if(actualUser.length != 0) {
+  //   actualUser.populate("courses")
+  //   .exec(function(err, doc) {
+  //     if(err) {console.log(err)};
+  //     console.log(doc[0]);
+  //     var results;
+  //     results = doc[0];
+  //     var hasCourses;
+  //     if(doc[0].courses.length) { //false if no courses
+  //       hasCourses = true;
+  //     } else {
+  //       hasCourses = false;
+  //     }
+  //     var sessionData = { "userData": results,
+  //                         "department": department,
+  //                         "number": number,
+  //                         "user": user, 
+  //                         "expand": false, 
+  //                         "progressTabFirst": false,
+  //                         "hasCourses": hasCourses};
+  //     console.log("user data is " + sessionData);
+  //     res.render('viewAssignments',sessionData);
+  //     return;
+  //   });
+  // } 
+  // else {
+  //   console.log('couldnt find user ' + username);
+  //   res.render('login',{error:"Could not find user " + username});
+  //   return;
+  // }
+  console.log(data);
+  data["progressTabFirst"] = false;
+  res.render('viewAssignments',data);
+
 };
 
+
+
+
 exports.viewAssignmentsTest = function(req, res){
+  // var user = req.session.user;
+  // console.log(user);
+  // if(!user) {
+  //   console.log('username undefined we should not do this');
+  //   res.render('login', {error:"Please sign in first!"});
+  //   return;
+  // }
+  
+  // //query database - get array of json situations
+  // var actualUser = models.User.find({"username": user});
+  // if(actualUser.length != 0) {
+  //   actualUser.populate("courses")
+  //   .exec(function(err, doc) {
+  //     if(err) {console.log(err)};
+  //     console.log(doc[0]);
+  //     var results;
+  //     results = doc[0];
+  //     var hasCourses;
+  //     if(doc[0].courses.length) { //false if no courses
+  //       hasCourses = true;
+  //     } else {
+  //       hasCourses = false;
+  //     }
+  //     var sessionData = { "userData": results,
+  //                         "department": department,
+  //                         "number": number,
+  //                         "user": user, 
+  //                         "expand": false, 
+  //                         "progressTabFirst": true,
+  //                         "hasCourses": hasCourses};
+  //     console.log("user data is " + sessionData);
+  //     res.render('viewAssignments',sessionData);
+  //     return;
+  //   });
+  // } 
+  // else {
+  //   console.log('couldnt find user ' + username);
+  //   res.render('login',{error:"Could not find user " + username});
+  //   return;
+  // }
   console.log(data);
   data["progressTabFirst"] = true;
   res.render('viewAssignments',data);
 };
+
+
 
 exports.viewInfo = function(req, res){
   console.log(data);
@@ -142,7 +177,10 @@ exports.viewIndex = function(req, res){
       } else {
         hasCourses = false;
       }
-      var sessionData = { "userData": results, "user": username, "expand": false, "hasCourses": hasCourses};
+      var sessionData = { "userData": results, 
+                          "user": username, 
+                          "expand": false, 
+                          "hasCourses": hasCourses};
       console.log("user data is " + sessionData);
       res.render('index',sessionData);
     });
@@ -160,8 +198,40 @@ exports.viewIndexExpand = function(req, res) {
 };
 
 exports.viewAddCoursePage = function(req, res) {
-  console.log(data);
-  res.render('addACourse',data);
+  var username = req.session.user;
+  console.log(username);
+  if(!username) {
+    console.log('username undefined we should not do this');
+    res.render('login', {error:"Please sign in first!"});
+    return;
+  }
+  //query database - get array of json situations
+  var actualUser = models.User.find({"username": username});
+  if(actualUser.length != 0) {
+    actualUser.populate("courses")
+    .exec(function(err, doc) {
+      if(err) {console.log(err)};
+      console.log(doc[0]);
+      var results;
+      results = doc[0];
+      var hasCourses;
+      if(doc[0].courses.length) { //false if no courses
+        hasCourses = true;
+      } else {
+        hasCourses = false;
+      }
+      var sessionData = { "userData": results, 
+                          "user": username, 
+                          "expand": false, 
+                          "hasCourses": hasCourses};
+      console.log("user data is " + sessionData);
+      res.render('addACourse',sessionData);
+    });
+  } else {
+    console.log('couldnt find user ' + username);
+    res.render('login',{error:"Could not find user " + username});
+    return;
+  }
 }
 
 exports.viewAddAssignmentPage = function(req, res) {
@@ -183,18 +253,16 @@ exports.viewAddAssignmentPage = function(req, res) {
       console.log(doc[0]);
       var results;
       results = doc[0];
-      // if (!doc.length) { //no result was found aka user isn't in the database aka user was "Guest"
-      //   results = data; 
-      // } else { //user is in the database, use their data
-      //   results = doc[0];
-      // }
       var hasCourses;
       if(doc[0].courses.length) { //false if no courses
         hasCourses = true;
       } else {
         hasCourses = false;
       }
-      var sessionData = { "userData": results, "user": user, "expand": false, "hasCourses": hasCourses};
+      var sessionData = { "userData": results, 
+                          "user": user, 
+                          "expand": false, 
+                          "hasCourses": hasCourses};
       console.log("user data is " + sessionData);
       res.render('addAssignment',sessionData);
       return;
