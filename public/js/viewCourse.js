e48828c12e377fabaf2e61de40c76619836e007b
Isabelle Woodrow <iwoodrow@stanford.edu>
2014-03-06 00:25:35 -0800

    working on editable tables ugh

--- public/js/viewCourse.js
+++ public/js/viewCourse.js
@@ -3,6 +3,31 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+	alert("here");
+	//editable stuff
+	$.fn.editable.defaults.mode = 'inline';
+
+	//make status editable
+    $('.type').editable({
+    	alert("test");
+        type: 'select',
+        title: 'Select status',
+        placement: 'right',
+        value: 2,
+        source: [
+            {value: 1, text: 'status 1'},
+            {value: 2, text: 'status 2'},
+            {value: 3, text: 'status 3'}
+        ]
+        /*
+        //uncomment these lines to send data on server
+        ,pk: 1
+        ,url: '/post'
+        */
+    });
+
+    $('.weight').editable();
 })
 
 /*
