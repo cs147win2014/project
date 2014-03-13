af0f3c725d2f8dfac0cd98b1440c4582fd96b1d8
Whitney LaRow <wlarow@stanford.edu>
2014-03-13 02:37:27 -0700

    rid of unnecessary stuff

--- routes/course.js
+++ routes/course.js
@@ -2,22 +2,6 @@ var data = require("../data.json");
 var models = require('../models');
 
 
-/* not currently in use
-exports.addAssignment = function(req, res) {    
-	// Your code goes here%
-	var newAssignment = {name: req.query.name, 
-					 type: req.query.type, 
-					 score: req.query.score,
-					 total: req.query.total};
-
-	data["assignments"].push(newAssignment);
-	
-	console.log(newAssignment);
-	// instead of rendering add screen, rendered data screen
-	res.render('editCourse',data);
- };
-*/
-
 exports.addSyllabusFields = function(req, res) {
 	allFields = req.body;
 
@@ -110,44 +94,6 @@ exports.getCourseSyllabus = function(req,res) {
 	    		} else {
 					console.log('here is the syllabus: ' + syllabus);
 					res.json(syllabus);
-				}
-				return;
-	    	});
-    	}
-  	} 
-  	else {
-    	console.log('couldnt find user ' + username);
-    	res.render('login',{error:"Could not find user " + username});
-    	return;
-  	}
-};
-
-exports.getCourseAssignments = function(req,res) {
-	var username = req.session.user;
-	console.log('username is: ' + username);
-	if(!username) {
-		console.log('username undefined we should not do this');
-		res.render('login', {error:"Please sign in first!"});
-		return;
-	}
-	var actualUser = models.User.find({"username": username});
-
-	if(actualUser.length != 0) {
-    	var courseID = req.params.courseID;
-    	console.log('heres the course ID: ' + courseID);
-
-    	var selectedCourseArray = models.Course.find({"_id":courseID});
-    	if (selectedCourseArray.length != 0) {
-	    	selectedCourseArray.populate("assignments")
-	    	.exec(function(err, populatedCourse) {
-	    		if(err) console.log(err);
-	    		var assignments = populatedCourse[0].assignments;
-	    		if(!assignments) {
-	    			console.log("NO ASSIGNMENTS FOUND");
-	    			res.json({});
-	    		} else {
-					console.log('here is ALL the assignments: ' + assignments);
-					res.json(assignments);
 				}
 				return;
 	    	});
