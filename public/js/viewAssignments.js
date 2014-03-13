ee340e9f90609fa24d537475693722d66b85aa7c
mnburkle <mnburkle@stanford.edu>
2014-03-13 15:08:55 -0700

    added in breakdown and clustered bar charts! :D!

--- public/js/viewAssignments.js
+++ public/js/viewAssignments.js
@@ -291,129 +291,163 @@ function callback(results) {
 function makeCharts(courseID) {
     console.log("making the charts!!!");
 
+
     $.get("/getAssignments/" + courseID, function(data) {
-        //console.log(data);
-        //console.log('now ill try to assign');
-
-        var chart = AmCharts.makeChart("testLine", {
-            "type": "serial",
-            "theme": "none",
-            "marginLeft": 20,
-            "pathToImages": "http://www.amcharts.com/lib/3/images/",
-            "legend": {
-                "equalWidths": false,
-                "periodValueText": "total: [[value.sum]]",
-                "position": "top",
-                "valueAlign": "left",
-                "valueWidth": 100
-            },
-            "dataProvider": data,
-            "valueAxes": [{
-                "minimum": 0,
-                "maximum": 100,
-                "axisAlpha": 0,
-                //"inside": true,
-                "position": "left",//,
-                //"ignoreAxisWidth": true
-                "title": "All Assignments"
-            }],
-            "graphs": [
-                {
-                    //"balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
-                    "bullet": "round",
-                    "bulletSize": 6,
-                    "lineColor": "#99C2FF",
-                    "lineThickness": 2,
-                    "negativeLineColor": "#637bb6",
-                    "type": "smoothedLine",
-                    "valueField": "score"
-                },
-                {
-                    //"balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
-                    "bullet": "round",
-                    "bulletSize": 6,
-                    "lineColor": "#d1655d",
-                    "lineThickness": 2,
-                    "negativeLineColor": "#637bb6",
-                    "type": "smoothedLine",
-                    "valueField": "total"
-                }
-            ],
-            "chartScrollbar": {},
-            "chartCursor": {
-                "categoryBalloonDateFormat": "YYYY",
-                "cursorAlpha": 0,
-                "cursorPosition": "mouse"
-            },
-            "categoryField": "name",
-            "categoryAxis": {
-                "startOnAxis": true,
-                "minorGridAlpha": 0.1,
-                "minorGridEnabled": true
-            }
-        });
+        console.log(data);
+        console.log('now ill try to assign');
         
+        var chartDivs = [];
+        var clusterChartData = [];
+        
+        var counter = 0;
+        for(var type in data) {
+            console.log('heres a thing i ndata');
+            console.log(type);
+            console.log('try get the ID now');
+            console.log($('#'+type+'breakdownChart').attr('id'));
 
-        var allAssignmentsChart = AmCharts.makeChart("allAssignmentsChartDiv", {
+            var clusterChartVal = {};
+            clusterChartVal["type"] = type;
+            clusterChartVal["actual"] = 0;
+            clusterChartVal["possible"] = 0;
+
+            for(var item in data[type]) {
+                //console.log('clustercharts stuff:');
+                //console.log(data[type][item]);
+                clusterChartVal["actual"] += data[type][item].score;
+                clusterChartVal["possible"] += data[type][item].total;
+            }
+            clusterChartVal["percent"] = clusterChartVal["actual"]/clusterChartVal["possible"];
+
+            clusterChartData[counter] = clusterChartVal;
+
+            var chartdivID = type+'breakdownChart';
+
+            if(data[type].length==0) {
+                $('#'+chartdivID).hide();
+            }
+            
+            else {
+            
+                /*breakdownLineChartTemplate.dataProvider = data[type];
+                breakdownLineChartTemplate.valueAxes[0].maximum = '[[value.sum]]';
+                console.log('making ' + type + 'with data: ');
+                console.log(breakdownLineChartTemplate.dataProvider);
+                chartDivs[counter] = AmCharts.makeChart(chartdivID, breakdownLineChartTemplate);*/
+
+                chartDivs[counter] = AmCharts.makeChart(chartdivID, {
+                    "type": "serial",
+                    "theme": "none",
+                    "marginLeft": 20,
+                    "pathToImages": "http://www.amcharts.com/lib/3/images/",
+                    "legend": {
+                        "equalWidths": false,
+                        "periodValueText": "Total: [[value.sum]]",
+                        "position": "top",
+                        "valueAlign": "left",
+                        "valueWidth": 100
+                    },
+                    "dataProvider": data[type],
+                    "valueAxes": [{
+                        "minimum": 0,
+                        "maximum": "[[value.sum]]",
+                        "axisAlpha": 0,
+                        //"inside": true,
+                        "position": "left",//,
+                        //"ignoreAxisWidth": true
+                        "title": "Total Points"
+                    }],
+                    "graphs": [
+                        {
+                            "balloonText": "Your Points: <b><span style='font-size:11px;'>[[value]]</span></b>",
+                            "bullet": "round",
+                            "bulletSize": 6,
+                            "lineColor": "#99C2FF",
+                            "lineThickness": 2,
+                            "negativeLineColor": "#637bb6",
+                            "type": "smoothedLine",
+                            "valueField": "score"
+                        },
+                        {
+                            "balloonText": "Possible Points: <b><span style='font-size:11px;'>[[value]]</span></b>",
+                            "bullet": "round",
+                            "bulletSize": 6,
+                            "lineColor": "#d1655d",
+                            "lineThickness": 2,
+                            "negativeLineColor": "#637bb6",
+                            "type": "smoothedLine",
+                            "valueField": "total"
+                        }
+                    ],
+                    "chartScrollbar": {},
+                    "chartCursor": {
+                        "categoryBalloonDateFormat": "YYYY",
+                        "cursorAlpha": 0,
+                        "cursorPosition": "mouse"
+                    },
+                    "categoryField": "name",
+                    "categoryAxis": {
+                        "startOnAxis": true,
+                        "minorGridAlpha": 0.1,
+                        "minorGridEnabled": true
+                    }
+                });            
+
+                counter++;
+                console.log('clustercharts stuff:');
+                console.log(clusterChartData);
+            }
+        }
+
+        var clusterChart = AmCharts.makeChart("clusterChartDiv", {
             "type": "serial",
             "theme": "none",
-            "pathToImages": "http://www.amcharts.com/lib/3/images/",
+            "columnWidth:": 0.6,
+            "columnSpacing": 5,
             "legend": {
                 "equalWidths": false,
-                "periodValueText": "total: [[value.sum]]",
+                "periodValueText": "Total: [[value.sum]]",
                 "position": "top",
                 "valueAlign": "left",
                 "valueWidth": 100
             },
-
-            "dataProvider": data,
-
-            "valueAxes": [{
-                "minimum": 0,
-                "maximum": 100, 
-                "axisAlpha": 0,
-                "gridAlpha": 0.07,
-                "position": "left",
-                "title": "Test plot! :D"
+            "dataProvider": clusterChartData,
+            "startDuration": 1,
+            "graphs": [{
+                "balloonText": "Actual:[[value]]",
+                "fillAlphas": 0.8,
+                "lineAlpha": 0.2,
+                "title": "Income",
+                "type": "column",
+                "valueField": "actual"
+            }, {
+                "balloonText": "Possible:[[value]]",
+                "fillAlphas": 0.8,
+                "lineAlpha": 0.2,
+                "title": "Expenses",
+                "type": "column",
+                "valueField": "possible"
+            }, {
+                "balloonText": "Percentage:[[value]]",
+                "fillAlphas": 0.8,
+                "lineAlpha": 0.2,
+                "title": "Income",
+                "type": "column",
+                "valueField": "percent"
             }],
-
-            "graphs": [
-                {
-                    "fillAlphas": 0.6,
-                    "lineAlpha": 0.4,
-                    "title": "Your score",
-                    "valueField": "score"
-                }, 
-                {
-                    "fillAlphas": 0.6,
-                    //"hidden": true,
-                    "lineAlpha": 0.4,
-                    "title": "Possible score",
-                    "valueField": "total"
-                }
-            ],
-
-            "plotAreaBorderAlpha": 0,
-            "marginTop": 10,
-            "marginLeft": 0,
-            "marginBottom": 0,
-            "chartScrollbar": {},
-            "chartCursor": {
-                "cursorAlpha": 0
-            },
-            "categoryField": "name",
+            "rotate": false,
+            "categoryField": "type",
             "categoryAxis": {
-                "startOnAxis": true,
-                "axisColor": "#DADADA",
-                "gridAlpha": 0.07
+                "gridPosition": "start",
+                "position": "left"
             },
-            "exportConfig": {
-                "menuTop": "10px",
-                "menuRight": "10px",
+            "exportConfig":{
+                "menuBottom":"20px",
+                "menuRight":"20px",
                 "menuItems": [{
-                    "icon": 'http://www.amcharts.com/lib/3/images/car.png',
-                    "format": 'png'
-                }]
+                "icon": '/lib/3/images/export.png',
+                "format": 'png'   
+                }]  
             }
         });
     });
